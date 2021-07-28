449b6cb4d1d908ad397ef26cd52ef93aed181a68
bur0chka <burashd124@gmail.com>
2021-07-28 18:11:59 +0500

    path 0.75
    Keystrokes are now triggered after the game ends, in the lower right corner there is now a patch name,
    now the target moves and reappears

--- gametarget.py
+++ gametarget.py
@@ -1,8 +1,7 @@
-import pygame, sys, random
+import pygame, sys, random, time
 from pygame.locals import *
 
 pygame.init()
-TEXTCOLOR = (0, 0, 0)
 def drawText(text, font, surface, x, y):
     textobj = font.render(text, 4, TEXTCOLOR)
     textrect = textobj.get_rect()
@@ -11,9 +10,10 @@ def drawText(text, font, surface, x, y):
 
 def gameover():
     windowSurface.fill(GREEN)
-    drawText('Игра окончена!', font, windowSurface, (300), (250))
-    drawText('счет:' + str(score), font, windowSurface, (300), (300))
-    drawText('Нажмите клавишу, чтобы играть снова', font, windowSurface, (300), (350))
+    drawText('Игра окончена!', font, windowSurface, (200), (250))
+    drawText('счет:' + str(score), font, windowSurface, (200), (300))
+    drawText('Нажмите клавишу, чтобы играть снова', font, windowSurface, (200), (350))
+    drawText('0.75', font, windowSurface, (725), (750))
     pygame.display.update()
 
 def presskey():
@@ -23,28 +23,38 @@ def presskey():
                 pygame.quit()
                 sys.exit()
             if event.type == KEYDOWN:
-                if event.key == K_ESCAPE: # Нажатие ESC осуществляет выход.
+                if event.key == K_ESCAPE:
                     pygame.quit()
                     sys.exit()
                 return
+
+def firing():
+    if listarrows != [] and arrows >= 0:
+        arrow.top -= 1
+        windowSurface.blit(arrowImage, arrow)
+        return True
+    else:
+        return False
 
 windowSurface = pygame.display.set_mode((800, 800), 1, 32)
 pygame.display.set_caption('Мишень')
 
 GREEN = (0, 200, 0)
 
-target = pygame.Rect(300, 150, 40, 40)
+TEXTCOLOR = (0, 0, 0)
+target = pygame.Rect(300, 50, 40, 40)
 bow = pygame.Rect(400, 650, 50, 50)
 arrow = pygame.Rect(300, 625, 40, 40)
 
 font = pygame.font.SysFont('1', 35)
 
 arrows = 10
-hitPoints = 0
 score = 0
 targets = 3
+x = 0
 listtargets = []
 listarrows = []
+listtargets.append(target)
 
 bowImage = pygame.image.load('bow.png')
 arrowImage = pygame.image.load('arrow.png')
@@ -53,35 +63,24 @@ bowRect = bowImage.get_rect()
 targetRect = targetImage.get_rect()
 arrowRect = arrowImage.get_rect()
 
-def firing():
-    if listarrows != [] and arrows > 0:
-        arrow.top -= 1
-        windowSurface.blit(arrowImage, arrow)
-        return True
-    else:
-        return False
-
-
-
-newtarget = {'draw': pygame.Rect(random.randint(0, 500 - 100), 0 - 40, 40, 40),
-             'surface': pygame.transform.scale(targetImage, (50, 50))}
-
-listtargets.append(target)
-
 while True:
+    x += 1
     windowSurface.fill(GREEN)
     bow.topleft = (250, 600)
-    target.topleft = (300, 50)
     windowSurface.blit(bowImage, bow)
     drawText('стрелы: ' + str(arrows), font, windowSurface, (20), (40))
     drawText('счет: ' + str(score), font, windowSurface, (20), (65))
+    drawText('0.75', font, windowSurface, (725), (750))
     if arrow.y == -50:
         listarrows.remove(arrow)
         arrow.y = 625
+    if x == 800 and listtargets != []:
+        x = -50
     if listtargets != []:
+        target = pygame.Rect(x, 50, 40, 40)
         windowSurface.blit(targetImage, target)
     firing()
-    if arrows > 0:
+    if arrows >= 0:
         drawText('стрелы: ' + str(arrows), font, windowSurface, (20), (40))
         drawText('счет: ' + str(score), font, windowSurface, (20), (65))
         pygame.display.update()
@@ -89,24 +88,32 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        if event.type == pygame.MOUSEBUTTONDOWN and arrows > 0 and listarrows == []:
+        if event.type == pygame.MOUSEBUTTONDOWN and arrows >= 0 and listarrows == []:
             pygame.mouse.get_rel()
             windowSurface.blit(arrowImage, arrow)
             arrows -= 1
             listarrows.append(arrow)
-    for targets in listtargets:
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+    for target in listtargets:
         if arrow.colliderect(target):
             score += 50
+            arrows += 2
             listtargets.remove(target)
             listarrows.remove(arrow)
+            x = -50
+            listtargets.append(target)
             arrow.y = 625
 
-    if arrows > 0:
+    if arrows >= 0:
         drawText('стрелы: ' + str(arrows), font, windowSurface, (20), (40))
         drawText('счет: ' + str(score), font, windowSurface, (20), (65))
-    if arrows > 0:
         pygame.display.update(arrow)
 
-    if arrows == 0:
+    if arrows == 0 and arrow.y == -49:
         gameover()
         presskey()
+        drawText('0.75', font, windowSurface, (725), (750))
+        arrows = 10
